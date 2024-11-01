d4a1202505b53de4457b7af5f1b265f5ba495590
rehanulhb <rehanulh@gmail.com>
2024-11-02 00:08:54 +0100

    Showing Bottles list from UI

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Bottle from "../Bottle/Bottle";
+import './Bottles.css'
 
 
 const Bottles = () => {
@@ -14,6 +16,11 @@ const Bottles = () => {
     return (
         <div>
             <h3>Bottles Showing Here : {bottles.length}</h3>
+            <div className="bottle-container">
+            {
+               bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>) 
+            }
+            </div>
         </div>
     );
 };
