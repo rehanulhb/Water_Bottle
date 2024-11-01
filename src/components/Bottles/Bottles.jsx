59d3d9b6f8dd861d231f454d2163ba8fe20ca57d
rehanulhb <rehanulh@gmail.com>
2024-11-01 23:54:58 +0100

    Added a Json File filled with Fake Data also created Bottles File and header file

---
+++ src/components/Bottles/Bottles.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Bottles = () => {
+    const [bottles, setBottles] = useState([]);
+
+    useEffect(()=>{
+        fetch('bottles.json')
+        .then(res => res.json())
+        .then(data=>setBottles(data));
+    }, [])
+
+    return (
+        <div>
+            <h3>Bottles Showing Here : {bottles.length}</h3>
+        </div>
+    );
+};
+
+export default Bottles;
