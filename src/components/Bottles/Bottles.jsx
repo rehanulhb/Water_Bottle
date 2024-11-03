ded5a084e62767a19404e9c92037ee6ac2f285a4
rehanulhb <rehanulh@gmail.com>
2024-11-03 02:39:01 +0100

    Set Handler and catch the data to show in Cart Item

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -6,6 +6,7 @@ import './Bottles.css'
 
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
+    const [cart, setCart] =useState([]);
 
     useEffect(()=>{
         fetch('bottles.json')
@@ -13,12 +14,18 @@ const Bottles = () => {
         .then(data=>setBottles(data));
     }, [])
 
+    const handleAddToCart = bottle =>{
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+    }
+
     return (
         <div>
-            <h3>Bottles Showing Here : {bottles.length}</h3>
+            <h3>Bottles Available : {bottles.length}</h3>
+            <h4>Cart: {cart.length}</h4>
             <div className="bottle-container">
             {
-               bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>) 
+               bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} handleAddToCart ={handleAddToCart}></Bottle>) 
             }
             </div>
         </div>
